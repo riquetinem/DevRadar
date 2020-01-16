cebbe3b6b3fcc42b58f19f0f5eab7a57fef73787
Henrique Tinem <henrique714tinem@gmail.com>
2020-01-16 14:18:13 -0300

    mobile

---
+++ mobile/src/pages/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+function Profile({ navigation }) {
+  const github_userName = navigation.getParam('github_username');
+  return <WebView style={{flex: 1}} source={{uri: `https://github.com/${github_userName}` }} />
+}
+
+export default Profile;
---
+++ mobile/src/routes.js
@@ -0,0 +1,34 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Main from './pages/Main'
+import Profile from './pages/Profile'
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Main: {
+      screen: Main,
+      navigationOptions: {
+        title: 'DevRadar'
+      },
+    },
+
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        title: 'Github'
+      },
+    },
+  }, {
+    defaultNavigationOptions: {
+      headerTintColor: '#FFF',
+      headerTitleAlign: 'center', // para colocar o titulo da app no centro
+      headerStyle: {
+        backgroundColor: '#7D40E7'
+      },
+      headerBackTitleVisible: null
+    }
+  })
+)
+
+export default Routes;
